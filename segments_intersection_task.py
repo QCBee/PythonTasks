453890d25e0992d8a86c09840e16f8c7e64b8585
QCBee <olgalyaschevskaya@gmail.com>
2021-01-18 18:33:40 +0200

    Added segments intersection task

--- segments_intersection_task.py
+++ segments_intersection_task.py
@@ -1,18 +1,19 @@
 # На числовой прямой даны два отрезка. Напишите программу, которая находит их пересечение
 # Гарантируется, что b1 > a1 and b2 > a2
-a1, a2, b1, b2 = int(input()), int(input()), int(input()), int(input())
-if a1 >= a2 and b1 >= b2:
+a1, b1, a2, b2 = int(input()), int(input()), int(input()), int(input())
+if a2 <= a1 <= b2 and a2 <= b1 <= b2:
     print(a1, b1)
-elif a2 > a1 and b2 > b2:
+elif a1 <= a2 <= b1 and a1 <= b2 <= b1:
     print(a2, b2)
-elif a1 != b2 or a2 != b1:
-    if a2 > a1 and b1 in (a2, b2):
-        print(a2, b1)
-    elif b2 in (a1, b1):
+elif (a2 <= a1 <= b2) and b1 > b2:
+    if a1 != b2:
         print(a1, b2)
-elif a1 == b2:
-    print(a1)
-elif a2 != b1:
-    print(a2)
+    else:
+        print(a1)
+elif (a1 <= a2 <= b1) and b2 > b1:
+    if a2 != b1:
+        print(a2, b1)
+    else:
+        print(a2)
 else:
     print('пустое множество')
